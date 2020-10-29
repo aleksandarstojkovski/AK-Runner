2d0569ee137eaa91a66ad3072ae424f2ac6520af
kenan.kasumovic <kenan.kasumovic@supsi.ch>
2020-10-29 11:08:57 +0100

    FIXED - Quit button in Ranking Scene

--- Assets/Scripts/RankingMenuScript.cs
+++ Assets/Scripts/RankingMenuScript.cs
@@ -2,22 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RankingMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void Quit() {
+    public void Quit() {
         SceneManager.LoadScene("MainMenu");
     }
 }
