d7ddd6c02222c0da5cdb1a47adce53047dfc0662
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-10-29 21:57:43 +0100

    pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+
+    bool Paused = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused == true)
+            {
+
+                Paused = false;
+            }
+            else
+            {
+
+                Paused = true;
+            }
+        }
+    }
+    public void Resume()
+    {
+
+    }
+
+}
