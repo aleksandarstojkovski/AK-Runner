8109546923bdacfe285b937421a5c9d34fa94e90
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-11-07 22:29:29 +0100

    fixed main menu

--- Assets/Scripts/MenuButtonController.cs
+++ Assets/Scripts/MenuButtonController.cs
@@ -6,35 +6,29 @@ public class MenuButtonController : MonoBehaviour {
 
 	// Use this for initialization
 	public int index;
-	[SerializeField] bool keyDown;
-	[SerializeField] int maxIndex;
+	public int maxIndex;
 	public AudioSource audioSource;
 
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
+		index = 0;
+		maxIndex = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetAxis ("Vertical") != 0){
-			if(!keyDown){
-				if (Input.GetAxis ("Vertical") < 0) {
-					if(index < maxIndex){
-						index++;
-					}else{
-						index = 0;
-					}
-				} else if(Input.GetAxis ("Vertical") > 0){
-					if(index > 0){
-						index --; 
-					}else{
-						index = maxIndex;
-					}
-				}
-				keyDown = true;
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			if(index < maxIndex){
+				index++;
+			}else{
+				index = 0;
 			}
-		}else{
-			keyDown = false;
+		} else if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			if(index > 0){
+				index --; 
+			} else {
+				index = maxIndex;
+			}
 		}
 	}
 
