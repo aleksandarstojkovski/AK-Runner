53d060c2b175d64d4bf8800a2d700686ad9a5858
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-11-18 17:07:52 +0100

    Added First Person and Third Person Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject firstPersonCam;
+    public GameObject thirdPersonCam;
+    int cameraMode = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (cameraMode == 3)
+                cameraMode = 1;
+            else
+                cameraMode = 3;
+        }
+        StartCoroutine(ChangeCamera());
+    }
+
+    IEnumerator ChangeCamera() {
+        yield return new WaitForSeconds(0.01f);
+        if (cameraMode == 1)
+        {
+            thirdPersonCam.SetActive(false);
+            firstPersonCam.SetActive(true);
+        }
+        else {
+            firstPersonCam.SetActive(false);
+            thirdPersonCam.SetActive(true);
+        }
+    }
+}
