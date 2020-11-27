8bd7f4a800417ebe54a2a501b7c874774896589c
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-11-27 16:26:50 +0100

    added fireworks to recordscore

--- Assets/Scripts/GameOverText.cs
+++ Assets/Scripts/GameOverText.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 public class GameOverText : MonoBehaviour
 {
 
+    public GameObject fireworks;
+
     void Start()
     {
         Text gameOverText = GetComponent<Text>();
+        float currentScore = PlayerPrefs.GetFloat(GamePrefs.Keys.CURRENT_SCORE);
+        float recordScoreForCurrentMap = PlayerPrefs.GetFloat(GamePrefs.Keys.CURRENT_MAP_RECORD_SCORE);
+        
         gameOverText.fontSize = 150;
-        gameOverText.text = "Your score: " + PlayerPrefs.GetFloat(GamePrefs.Keys.CURRENT_SCORE) + "\n" + "⋆ Best score for this map: " + PlayerPrefs.GetFloat(GamePrefs.Keys.CURRENT_MAP_RECORD_SCORE) + " ⋆";
+        gameOverText.text = "Your score: " + currentScore + "\n" + "⋆ Best score for this map: " + recordScoreForCurrentMap + " ⋆";
+
+        if (currentScore == recordScoreForCurrentMap) {
+            fireworks.SetActive(true);
+        }
     }
 
     void Update()
