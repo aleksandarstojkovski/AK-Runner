54fd46fc9642c7817bcaa6e0565fd782ea1f4d54
kenan.kasumovic <kenan.kasumovic@supsi.ch>
2020-12-05 19:26:20 +0100

    ADDED - instructions on main menu

---
+++ Assets/Scripts/MainMenuHelpController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHelpController : MonoBehaviour
+{
+    [SerializeField] GameObject instructionPanel;
+    [SerializeField] bool isPressed = false;
+    
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void enableDisablePanel()
+    {
+        isPressed = !isPressed;
+        instructionPanel.SetActive(isPressed);
+    }
+}
